19a062902a18b9465669b644b16e351fc06a838e
ericanthonywu <ericanthonywu89@gmail.com>
2019-12-20 12:15:21 +0700

    change code optimizing

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,23 @@ const router = express.Router();
 const ig = require('instagram-scraping')
 const moment = require('moment')
 const {ig: Post} = require('../model')
+const mongoose = require('mongoose')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res) => {
     ig.deepScrapeTagPage('ootd').then(data => {
+        const postData = []
         data.medias.forEach(data => {
-            Post.create({
-                username: data.owner.username,
+            postData.push({
+                username: data.owner.username || "",
                 image: data.display_url,
                 like: data.edge_media_preview_like.count,
                 comment: data.edge_media_to_parent_comment.count > data.edge_media_preview_comment.count ? data.edge_media_to_parent_comment.count : data.edge_media_preview_comment.count,
                 tanggalPosting: moment(data.taken_at_timestamp).toISOString()
-            }).catch(err => console.log(err))
+            })
         })
-        res.status(200).json(data)
-    })
+        Post.insertMany(postData).then(() => res.status(200).json()).catch(err => console.log(err)).finally(() => mongoose.connection.close())
+    }).catch(err => res.status(500).json(err))
+
 })
 
 module.exports = router;
